58b0f3e3f9479420e4b4248a4c4e59232ee3512e
Leonardo Ribeiro Braz <f0995076@unincor.br>
2023-11-24 17:32:26 -0300

    arquivos atualizados

--- java_poo.js
+++ java_poo.js
@@ -1,16 +1,35 @@
 
-const pessoa = {
-    nome: 'Leonardo Ribeiro',
-    idade: 25,
+class Pessoa {
+    nome;
+    idade;
+    anoNascimento;
 
-    descrever: function () {
-        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+        this.anoNascimento = 2022 - idade;
     }
-};
 
-const atributo = 'idade';
+    descrever() {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade} anos.`);
+    }
+}
 
-console.log(pessoa[`nome`]);
+const vitor = new Pessoa('Vitor', 25);
+//vitor.nome = 'Vitor J Guerra';
+//vitor.idade = 25;
+//vitor.descrever();
+
+const renan = new Pessoa('Renan', 30);
+//renan.nome = 'Renan J Guerra';
+//renan.idade = 28;
+//renan.descrever();
+
+console.log(vitor);
+
+//const atributo = 'idade';
+
+//console.log(pessoa[`nome`]);
 
 //pessoa.nome = 'Leor';
 //pessoa.idade = 30;
---
+++ java_poo01.js
@@ -0,0 +1,20 @@
+class Pessoa {
+}
+
+function compararPessoas(p1, p2){
+    if (p1.idade > p2.idade){
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`);
+    } else if (p2.idade > p1.idade) {
+        console.log(`${p2.nome} é mais velho(a) que ${p1.nome}`);
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade.`);
+    }        
+}
+
+const vitor = new Pessoa('Vitor', 25);
+const renan = new Pessoa('Renan', 30);
+
+compararPessoas(vitor, renan);
+
+
+
