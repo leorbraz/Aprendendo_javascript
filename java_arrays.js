a93f1b598daf352aa062c76de91a292db2caf3f6
Leonardo Ribeiro Braz <f0995076@unincor.br>
2023-12-04 17:03:53 -0300

    atualizando dois arquivos

---
+++ java_arrays.js
@@ -0,0 +1,16 @@
+
+//Criando uma lista
+const alunos = ['João', 'Carlos', 'Pedro']; 
+
+//Adicionar itens a lista
+alunos.push('Patrick'); 
+
+// Adicionar diretamente na posição 
+alunos[4] = 'Vinicius';
+
+//remove para fora o último item da lista
+console.log(alunos.pop()); 
+
+// remove para fora o primeiro item da lista
+console.log(alunos.shift()); 
+
